225efed711121d5d89077c5f834cd819dc27f29a
Navin Joshi <naveenj.iteg2022@ssism.org>
2024-04-13 11:05:05 +0530

    utils code

--- src/db/index.js
+++ src/db/index.js
@@ -3,8 +3,12 @@ import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-    console.log(`MongoDB Connected to HOST! ${mongoose.connection.host}`);
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
+    console.log(
+      `MongoDB Connected to HOST! ${connectionInstance.connection.host}`
+    );
   } catch (error) {
     console.error("MONGODB Connection FAILED", error);
     process.exit(1);
--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 dotenv.config();
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (err) => {
+      console.log(err);
+      throw err;
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log("Server listening on port " + process.env.PORT);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB db Connection failed !!" + err);
+  });
