f9e5187b56d1d606f3c91320f28a16070dc7889c
Navin Joshi <naveenj.iteg2022@ssism.org>
2024-04-16 14:21:02 +0530

    add some controllers

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,11 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import {
+  loginUser,
+  logoutUser,
+  registerUser,
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
-import { getUsers } from "../controllers/user.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router();
 
 router.route("/register").post(
@@ -11,5 +15,10 @@ router.route("/register").post(
   ]),
   registerUser
 );
-router.route("/get").get(getUsers)
+
+router.route("/login").post(loginUser);
+
+// secured Routes
+
+router.route("/logout").post(verifyJWT, logoutUser);
 export default router;
