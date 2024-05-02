a1a93aeb13e0a0de2594d6eae9093b2d08ea6adc
Navin Joshi <naveenj.iteg2022@ssism.org>
2024-05-02 11:21:41 +0530

    add getwatch history endpoint

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,7 @@ import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
 const generateAccessAndRefreshTokens = async (userId) => {
   try {
     const user = await User.findById(userId);
@@ -365,6 +366,57 @@ const getUserChannerProfile = asyncHandler(async (req, res) => {
     );
 });
 
+const getWatchHistory = asyncHandler(async (req, res) => {
+  const user = await User.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(req.user._id),
+      },
+    },
+    {
+      $lookup: {
+        from: "videos",
+        localField: "watchHistory",
+        foreignField: "_id",
+        as: "watchHistory",
+        pipeline: [
+          {
+            $lookup: {
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "owner",
+              pipeline: [
+                {
+                  $project: {
+                    fullName: 1,
+                    username: 1,
+                    avatar: 1,
+                  },
+                },
+              ],
+            },
+          },
+          {
+            owner: {
+              $first: "$owner",
+            },
+          },
+        ],
+      },
+    },
+  ]);
+  return res.status(
+    (200).json(
+      new ApiResponse(
+        200,
+        user[0].watchHistory,
+        "watch history fetched successfully "
+      )
+    )
+  );
+});
+
 export {
   registerUser,
   loginUser,
@@ -376,4 +428,5 @@ export {
   updateUserAvatar,
   updateUserCoverImage,
   getUserChannerProfile,
+  getWatchHistory
 };
