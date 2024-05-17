cd646fcbcfdbcf76fd71b9f3b8e322307b9802a0
Navin Joshi <naveenj.iteg2022@ssism.org>
2024-05-17 16:44:07 +0530

    update in user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,6 +5,7 @@ import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken";
 import mongoose from "mongoose";
+
 const generateAccessAndRefreshTokens = async (userId) => {
   try {
     const user = await User.findById(userId);
